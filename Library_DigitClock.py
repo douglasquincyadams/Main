15f2550343bea1338e1a6e85a45591164e256f93
douglasquincyadams <douglasquincyadams@gmail.com>
2017-05-25 13:21:14 -0400

    Updated comments and added an example

--- Library_DigitClock.py
+++ Library_DigitClock.py
@@ -6,6 +6,22 @@ DEFINITION:
     given a number of differnt independent events 
     accross differnt dimensions
 
+    Though this really is an outer product, it was written in my sophmore year of college
+    within the context of counting in different bases for each digit of a clock. 
+    
+    For example:
+        if you wanted to count in binary, up to the number   11111
+        then you would pass in :
+            [1,1,1,1,1]
+
+        if you wanted to count in base 10 up to 100
+        then you would pass in :
+            [9,9]
+
+    the thought was that you would allow up to some maximum number for each digit.
+    
+    It was later that I realized this was the same thing as an outer product.
+
 ARGS:
     DigitMaximums
         Type: Python List of Python Ints
@@ -13,13 +29,41 @@ ARGS:
             Contains a list of the number of possibilities within each dimension - 1.
             i.e
                 1,2,3
-            has possiblities:
+            has possiblities for each index:
                 [0,1]  [0,1,2]  [0,1,2,3]
 
 
 RETURNS:
     A list of every possibile combination of possibliities
-
+        i.e the arg:
+            1,2,3
+        would return:
+        [
+        [0, 0, 0]
+        [1, 0, 0]
+        [0, 1, 0]
+        [1, 1, 0]
+        [0, 2, 0]
+        [1, 2, 0]
+        [0, 0, 1]
+        [1, 0, 1]
+        [0, 1, 1]
+        [1, 1, 1]
+        [0, 2, 1]
+        [1, 2, 1]
+        [0, 0, 2]
+        [1, 0, 2]
+        [0, 1, 2]
+        [1, 1, 2]
+        [0, 2, 2]
+        [1, 2, 2]
+        [0, 0, 3]
+        [1, 0, 3]
+        [0, 1, 3]
+        [1, 1, 3]
+        [0, 2, 3]
+        [1, 2, 3]
+        ]
 
 """
 
